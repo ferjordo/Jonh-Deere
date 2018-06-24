9e497822d9e4c9347457ae6f3f05f9c0acfd622a
ferjordo <ferjordo2011@gmail.com>
2018-06-23 23:58:52 -0500

    Primer commit
    Creación del proyeto

---
+++ Jonh Deere/Assets/Script/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//Use to change the scene
+	public void GoToScene(string scenename){
+        SceneManager.LoadScene(scenename);
+	}
+}
